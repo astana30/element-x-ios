dec5e7edf62155cfb997c58f95bcf906eb2cc134
Zorrx <astana300027@gmail.com>
2026-03-11 20:05:26 +0500

    Rebrand: finalize SalemX app identity

--- ElementX/Sources/Application/Settings/AppSettings.swift
+++ ElementX/Sources/Application/Settings/AppSettings.swift
@@ -199,7 +199,7 @@ final class AppSettings {
     private(set) var hideBrandChrome = false
     
     /// The task identifier used for background app refresh. Also used in main target's the Info.plist
-    let backgroundAppRefreshTaskIdentifier = "io.element.elementx.background.refresh"
+    let backgroundAppRefreshTaskIdentifier = "kz.salemx.msg.background.refresh"
 
     /// A URL where users can go read more about the app.
     private(set) var websiteURL: URL = "https://element.io"
