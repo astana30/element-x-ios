cbf38af6f7d5af9961a72dc0269cc7e989a3aa2c
Zorrx <astana300027@gmail.com>
2026-03-14 13:04:16 +0500

    UI: remove top settings button from home screen

--- ElementX/Sources/Screens/HomeScreen/View/HomeScreen.swift
+++ ElementX/Sources/Screens/HomeScreen/View/HomeScreen.swift
@@ -52,11 +52,6 @@ struct HomeScreen: View {
         
     @ToolbarContentBuilder
     private var toolbar: some ToolbarContent {
-        ToolbarItem(placement: .navigationBarLeading) {
-            settingsButton
-                .buttonStyle(.borderless)
-        }
-        
         ToolbarItem(placement: .primaryAction) {
             if #available(iOS 26, *) {
                 newRoomButton
@@ -79,23 +74,6 @@ struct HomeScreen: View {
                                          in: navigationTransitionNamespace)
             }
         }
-    }
-    
-    private var settingsButton: some View {
-        Button {
-            context.send(viewAction: .showSettings)
-        } label: {
-            LoadableAvatarImage(url: context.viewState.userAvatarURL,
-                                name: context.viewState.userDisplayName,
-                                contentID: context.viewState.userID,
-                                avatarSize: .user(on: .chats),
-                                mediaProvider: context.mediaProvider)
-                .accessibilityIdentifier(A11yIdentifiers.homeScreen.userAvatar)
-                .clipShape(.circle)
-                .overlayBadge(10, isBadged: context.viewState.requiresExtraAccountSetup)
-                .compositingGroup()
-        }
-        .accessibilityLabel(L10n.commonSettings)
     }
     
     @ViewBuilder
